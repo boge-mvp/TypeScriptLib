bb1e48eedd68e298f73ffdc944c13bc51db59d8b
boge <1037195898@qq.com>
2026-05-29 15:27:32 +0800

    refactor(View): 移除废弃的getChild方法
    - 删除了废弃的getChild方法及其相关注释
    - 保留了getChildByNames方法作为替代方案
    - 清理了不再使用的GObject导入声明

--- TSCore/src/com/core/View.ts
+++ TSCore/src/com/core/View.ts
@@ -3,22 +3,11 @@ import {StringBlock} from "../block/StringBlock";
 import {ViewBlock} from "../block/ViewBlock";
 import {ActionEvent} from "../block/ActionEvent";
 import GComponent = fgui.GComponent;
-import GObject = fgui.GObject;
 import {IView} from "../interfaces/IView";
 
 export class View extends mixinExt(ActionEvent, StringBlock, ViewBlock, GComponent) implements IView, IKey {
 
     protected key: string
-
-    /**
-     * 获取子组件
-     * @param name 传入子组件多种命名方式
-     * @deprecated
-     * @see getChildByNames
-     */
-    override getChild<T = GObject>(...name: string[]): T {
-        return this.getChildByNames(...name)
-    }
 
     setKey(key: string) {
         this.key = key
